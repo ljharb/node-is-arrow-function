9fda7f7d6363a6285d31f0201952161a0bff8f30
lionel-rowe <lionel.rowe@gmail.com>
2024-05-09 17:25:54 +0800

    More robust logic for various edge cases

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 'use strict';
 
-var isCallable = require('is-callable');
-var fnToStr = Function.prototype.toString;
-var isNonArrowFnRegex = /^\s*function/;
-var isArrowFnWithParensRegex = /^\([^)]*\) *=>/;
-var isArrowFnWithoutParensRegex = /^[^=]*=>/;
+/** @param {number} n */
+var found = function (n) {
+	return n !== -1;
+};
 
+/** @param {unknown} fn */
 module.exports = function isArrowFunction(fn) {
-	if (!isCallable(fn)) {
+	if (typeof fn !== 'function') {
 		return false;
 	}
-	var fnStr = fnToStr.call(fn);
-	return fnStr.length > 0
-		&& !isNonArrowFnRegex.test(fnStr)
-		&& (isArrowFnWithParensRegex.test(fnStr) || isArrowFnWithoutParensRegex.test(fnStr));
+
+	/** @type {string} */
+	var fnStr = Function.prototype.toString.call(fn);
+
+	var classRe = /^\s*class[\s/{]/;
+	var stripRe = /^\s+|\s+$/g;
+
+	var firstNonSpace = fnStr.search(/\S/);
+	var quote = fnStr.search(/['"`]/);
+
+	var paren = fnStr.indexOf('(');
+	var brace = fnStr.indexOf('{');
+	var arrow = fnStr.indexOf('=>');
+
+	if (classRe.test(fnStr)) {
+		return false;
+	}
+	if (!found(arrow) || !found(firstNonSpace)) {
+		return false;
+	}
+	if (!found(brace) || !found(paren)) {
+		return true;
+	}
+	if (found(quote) && quote === firstNonSpace) {
+		return false;
+	}
+	if (paren === firstNonSpace) {
+		return true;
+	}
+	if (Math.min(arrow, brace, paren) === arrow) {
+		return true;
+	}
+
+	var beforeParams = fnStr.slice(0, paren).replace(stripRe, '');
+	if (beforeParams && beforeParams !== 'async') {
+		return false;
+	}
+
+	return paren < arrow && arrow < brace;
 };
--- test/index.js
+++ test/index.js
@@ -1,14 +1,10 @@
 'use strict';
 
-var test = require('tape');
-var isArrowFunction = require('../index');
-var arrowFuncs = require('make-arrow-function').list().slice(0, -2);
-var asyncFuncs = require('make-async-function').list();
-var generatorFuncs = require('make-generator-function')();
-var forEach = require('for-each');
+const test = require('tape');
+const isArrowFunction = require('../index');
 
 test('returns false for non-functions', function (t) {
-	var nonFuncs = [
+	const nonFuncs = [
 		true,
 		false,
 		null,
@@ -17,70 +13,341 @@ test('returns false for non-functions', function (t) {
 		[],
 		/a/g,
 		'string',
+		'() => {}',
+		'function () {}',
 		42,
-		new Date()
+		new Date(0),
 	];
+
 	t.plan(nonFuncs.length);
-	forEach(nonFuncs, function (nonFunc) {
+	for (const nonFunc of nonFuncs) {
 		t.notOk(isArrowFunction(nonFunc), nonFunc + ' is not a function');
-	});
+	}
 	t.end();
 });
 
 test('returns false for non-arrow functions', function (t) {
-	var func = function () {};
-	t.notOk(isArrowFunction(func), 'anonymous function is not an arrow function');
+	const fns = [
+		function () {},
+		function foo() {},
+		function foo() { '=>' },
+		function foo() { () => {} },
+	]
 
-	var namedFunc = function foo() {};
-	t.notOk(isArrowFunction(namedFunc), 'named function is not an arrow function');
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.notOk(isArrowFunction(fn), String(fn) + ' is not an arrow function');
+	}
+	t.end();
+});
 
-	/* globals window */
-	if (typeof window === 'undefined') {
-		t.skip('window.alert is not an arrow function');
+test('returns false for built-ins', function (t) {
+	const fns = [
+		setTimeout,
+		Date,
+		Date.now,
+		Promise,
+		Promise.all,
+		Intl.Collator,
+		eval,
+		globalThis.eval,
+	]
+
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.notOk(isArrowFunction(fn), String(fn) + ' is a built-in');
+	}
+
+	t.end();
+});
+
+test('returns false for alert', function (t) {
+	if (typeof globalThis.alert !== 'function') {
+		t.skip();
 	} else {
-		t.notOk(isArrowFunction(window.alert), 'window.alert is not an arrow function');
+		t.notOk(isArrowFunction(globalThis.alert));
+	}
+	t.end();
+});
+
+test('returns false for document.all', function (t) {
+	if (typeof document === 'undefined') {
+		t.skip();
+	} else {
+		t.notOk(isArrowFunction(document.all));
 	}
 	t.end();
 });
 
 test('returns false for non-arrow function with faked toString', function (t) {
-	var func = function () {};
-	func.toString = function () { return 'ARROW'; };
+	const func = function () {};
+	func.toString = function () { return '() => {}'; };
 
 	t.notEqual(String(func), Function.prototype.toString.call(func), 'test function has faked toString that is different from default toString');
-	t.notOk(isArrowFunction(func), 'anonymous function with faked toString is not an arrow function');
+	t.notOk(isArrowFunction(func), 'non-arrow function with faked toString is not an arrow function');
+	t.end();
+});
+
+test('returns true for arrow function with faked toString', function (t) {
+	const func = () => {};
+	func.toString = function () { return 'function () {}'; };
+
+	t.notEqual(String(func), Function.prototype.toString.call(func), 'test function has faked toString that is different from default toString');
+	t.ok(isArrowFunction(func), 'arrow function with faked toString is an arrow function');
 	t.end();
 });
 
 test('returns true for arrow functions', function (t) {
-	if (arrowFuncs.length > 0) {
-		arrowFuncs.forEach(function (arrowFunc) {
-			t.ok(isArrowFunction(arrowFunc), 'arrow function ' + arrowFunc + ' is arrow function');
-		});
-	} else {
-		t.skip('arrow function is arrow function - this environment does not support ES6 arrow functions. Please run `node --harmony`, or use a supporting browser.');
+	const fns = [
+		(a, b) => a * b,
+		() => 42,
+		() => function () {},
+		() => x => x * x,
+		y => x => x * x,
+		x => x * x,
+		x => { return x * x; },
+		(x, y) => { return x + x; },
+		(a = Math.random(10)) => {},
+		(a = function () {
+			if (Math.random() < 0.5) { return 42; }
+			return "something else";
+		}) => a(),
+	];
+
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.ok(isArrowFunction(fn), String(fn) + ' is arrow function');
 	}
 	t.end();
 });
 
 test('returns true for async arrow functions', function (t) {
-	if (asyncFuncs.length > 0) {
-		forEach(asyncFuncs.slice(0, 2), function (asyncFunc) {
-			t.ok(isArrowFunction(asyncFunc), 'async arrow function ' + asyncFunc + ' is arrow function');
-		});
-		forEach(asyncFuncs.slice(2), function (asyncFunc) {
-			t.notOk(isArrowFunction(asyncFunc), 'async non-arrow function ' + asyncFunc + ' is not an arrow function');
-		});
-	} else {
-		t.skip('async arrow function is arrow function - this environment does not support ES2017 async functions. Please run `node --harmony`, or use a supporting browser.');
+	const fns = [
+		async (a, b) => a * b,
+		async x => {},
+		async () => {},
+		async () => { function f() {} },
+	];
+
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.ok(isArrowFunction(fn), String(fn) + ' is async arrow function');
 	}
 	t.end();
 });
 
-test('generators', { skip: generatorFuncs.length === 0 }, function (t) {
-	forEach(generatorFuncs, function (genFn) {
-		t.notOk(isArrowFunction(genFn), 'generator function ' + genFn + ' is not an arrow function');
-	});
+test('returns false for async non-arrow functions', function (t) {
+	const fns = [
+		async function () {},
+		async function foo() {},
+		async function () { '=>' },
+		async function foo() { '=>' },
+	];
+
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.notOk(isArrowFunction(fn), String(fn) + ' is async non-arrow function');
+	}
+	t.end();
+});
+
+test('returns false for generator functions', function (t) {
+	const fns = [
+		function* () { const x = yield; return x || 42; },
+		function* gen() { const x = yield; return x || 42; },
+		({ *       concise() { const x = yield; return x || 42; } }).concise,
+	];
+
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.notOk(isArrowFunction(fn), String(fn) + ' is async non-arrow function');
+	}
+	t.end();
+});
+
+test('returns false for async generator functions', function (t) {
+	const fns = [
+		async function* () {},
+		async function* () { yield '=>' },
+	];
+
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.notOk(isArrowFunction(fn), String(fn) + ' is async non-arrow function');
+	}
+	t.end();
+});
+
+test('returns true for arrow function properties', function (t) {
+	const fns = [
+		({
+			prop: () => {}
+		}).prop,
+		({
+			prop: async () => {}
+		}).prop,
+		({
+			prop: () => { function x() { } }
+		}).prop,
+		({
+			prop: x => { function x() { } }
+		}).prop,
+		({
+			function: () => {}
+		}).function,
+		({
+			'': () => {}
+		})[''],
+	];
+
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.ok(isArrowFunction(fn), String(fn) + ' is arrow prop');
+	}
+	t.end();
+});
+
+test('returns false for non-arrow function properties', function (t) {
+	const fns = [
+		({
+			prop: function () {}
+		}).prop,
+		({
+			prop: async function () {}
+		}).prop,
+		({
+			prop: function* () {}
+		}).prop,
+		({
+			prop: async function* () {}
+		}).prop,
+		({
+			prop: function () { () => {} }
+		}).prop,
+		({
+			'=>': function () {}
+		})['=>'],
+		({
+			'': function () {}
+		})[''],
+	];
+
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.notOk(isArrowFunction(fn), String(fn) + ' is non-arrow prop');
+	}
+	t.end();
+});
+
+test('returns false for methods', function (t) {
+	const fns = [
+		({
+			method() {}
+		}).method,
+		({
+			async method() {}
+		}).method,
+		({
+			*method() {}
+		}).method,
+		({
+			async *method() {}
+		}).method,
+		({
+			method() { return '=>' }
+		}).method,
+		({
+			method() { () => {} }
+		}).method,
+		({
+			'=>'() {}
+		})['=>'],
+		({
+			''() {}
+		})[''],
+	];
+
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.notOk(isArrowFunction(fn), String(fn) + ' is method');
+	}
+	t.end();
+});
+
+test('returns false for classes', function (t) {
+	const fns = [
+		class {},
+		class X { },
+		class { '=>' },
+		class X { m() { return '=>' } },
+		class X { p = () => {} },
+	];
+
+	t.plan(fns.length);
+	for (const fn of fns) {
+		t.notOk(isArrowFunction(fn), String(fn) + ' is class');
+	}
+	t.end();
+});
+
+test('https://github.com/inspect-js/is-arrow-function/issues/26', function (t) {
+	// False positive on object methods with arrow functions inside
+	const x = {
+		foo() {
+			return 42
+		},
+		bar() {
+			return (() => 10)()
+		},
+		buz() {
+			// if this was an arrow function, it would have included => in it
+			return 123
+		},
+	};
+
+	t.notOk(isArrowFunction(x.foo));
+	t.notOk(isArrowFunction(x.bar));
+	t.notOk(isArrowFunction(x.buz));
+
+	t.end();
+});
+
+test('https://github.com/inspect-js/is-arrow-function/issues/15', function (t) {
+	// False negative for arrow function with closing parenthesis in parameter list
+
+	const arrow = (a = Math.random(10)) => {};
+	t.ok(isArrowFunction(arrow));
+
+	const arrow2 = (a = function () {
+		if (Math.random() < 0.5) {
+			return 42;
+		}
+		return "something else";
+	}) => a();
+
+	t.ok(isArrowFunction(arrow2));
+
+	t.end();
+});
+
+test('corner cases', function (t) {
+	t.ok(isArrowFunction((a = function () {}) => a()));
+
+	t.notOk(isArrowFunction(function (a = () => {}) {
+		return a();
+	}));
+
+	t.ok(isArrowFunction(({
+		'function name': () => {}
+	})['function name']));
+
+	t.ok(isArrowFunction(({
+		function文: () => { }
+	})['function文']));
+
+	t.notOk(isArrowFunction(({
+		'() => {}'() {}
+	})['() => {}']));
 
 	t.end();
 });
